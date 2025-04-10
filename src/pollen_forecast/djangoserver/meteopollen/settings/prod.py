f40382629b9ce8c94a66bfdaa8f54b011592eda8
Antoine TAVANT <antoinetavant@hotmail.fr>
2025-04-10 06:58:41 +0200

    update prod settings

--- src/pollen_forecast/djangoserver/meteopollen/settings/prod.py
+++ src/pollen_forecast/djangoserver/meteopollen/settings/prod.py
@@ -4,19 +4,11 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ["pollen.antoinetavant.fr"]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-    }
-}
-
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
